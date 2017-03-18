e5d2ba2d2c5b31248fbf60d1524fb827c291960d
Mariusler <mariuler@stud.ntnu.no>
2017-03-18 22:24:45 +0100

    Framework done
    Signed-off-by: Mariusler <mariuler@stud.ntnu.no>

--- Project/client.go
+++ Project/client.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
-	"messages"
 	"net"
 	"os"
 	"strings"
 	"time"
+
+	"./msg"
 )
+
+func main() {
+	server := "127.0.0.1:30000"
+
+	conn := connectToServer(server)
+	defer conn.Close()
+
+	inputCh := make(chan string)
+	incommingMsgCh := make(chan messages.ServerPayload)
+
+	go inputListener(inputCh)
+	go messageListener(incommingMsgCh, conn)
+
+	for {
+		select {
+		case input := <-inputCh: // Set up msg, json then send it
+			msg := handleInput(input)
+			bytes, err := json.Marshal(msg)
+			if err != nil {
+				fmt.Println("Error json marshall", err)
+			}
+			conn.Write(bytes)
+		case msg := <-incommingMsgCh:
+			fmt.Println("msg received yo", msg)
+		}
+	}
+}
 
 func inputListener(inputCh chan<- string) {
 	reader := bufio.NewReader(os.Stdin)
@@ -20,6 +49,24 @@ func inputListener(inputCh chan<- string) {
 		}
 		input = strings.Replace(input, "\n", "", -1) // will work on unix and windows
 		inputCh <- input
+	}
+}
+
+func messageListener(incommingMsgCh chan<- messages.ServerPayload, conn net.Conn) {
+	buffer := make([]byte, 2048)
+	var msg messages.ServerPayload
+	for {
+		bytes, err := conn.Read(buffer)
+		if err != nil {
+			conn.Close()
+			return
+		}
+		errr := json.Unmarshal(buffer[:bytes], msg)
+		if errr != nil {
+			fmt.Println("Error Unmarshall", err)
+			continue
+		}
+		incommingMsgCh <- msg
 	}
 }
 
@@ -35,35 +82,18 @@ func connectToServer(ipAndPort string) net.Conn {
 }
 
 func handleInput(input string) messages.ClientPayload {
-	splitInput = strings.Split(input, " ")
+	splitInput := strings.Split(input, " ")
 	var msg messages.ClientPayload
-	msg.Req
-	req_beg := strings.Index(input, "\\")
-	if req_beg == 0 {
-		req_end := strings.Index(input, " ")
-		msg.Request = input[req_begin+1 : req_end]
-		msg.Content = input[req_end+1:]
+	reqBeg := strings.Index(input, "\\")
+	if reqBeg == 0 {
+		reqEnd := strings.Index(input, " ")
+		msg.Request = input[reqBeg+1 : reqEnd]
+		if len(splitInput) != 1 {
+			msg.Content = input[reqEnd+1:]
+		}
 	} else {
 		msg.Request = "msg"
 		msg.Content = "input"
 	}
-}
-
-func main() {
-	server := "127.0.0.1:30000"
-
-	conn := connectToServer(server)
-	defer conn.Close()
-
-	inputCh := make(chan string)
-
-	go inputListener(inputCh)
-
-	for {
-		select {
-		case input := <-inputCh: // Set up msg, json then send it
-			msg := handleInput(input)
-
-		}
-	}
+	return msg
 }
--- Project/server.go
+++ Project/server.go
@@ -1,19 +1,79 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
 
-
-
-
+	"./msg"
+)
 
 func main() {
-  ln, err := net.Listen("tcp","127.0.0.1:30000")
-  if err != nil {
-    fmt.Println("Error")
-    os.Exit(1)
-  }
-  defer ln.close
+	clients := make(map[net.Conn]string)
+	//msgHistory := make([]messages.ServerPayload)
 
-  
+	newConnCh := make(chan net.Conn)
+	inCommingMsgCh := make(chan messages.ClientPayload)
 
+	go connListener(newConnCh)
 
+	for {
+		select {
+		case newConn := <-newConnCh:
+			clients[newConn] = ""
+			go clientListener(inCommingMsgCh, newConn)
+			fmt.Println("New client")
+			fmt.Println(clients)
+		case msg := <-inCommingMsgCh:
+			switch msg.Request {
+			case "login":
+			case "logout":
+			case "names":
+			case "help":
+			default:
+
+			}
+		}
+	}
+}
+
+func connListener(newConnCh chan<- net.Conn) {
+	ln, err := net.Listen("tcp", "127.0.0.1:30000")
+	if err != nil {
+		fmt.Println("Error")
+		os.Exit(1)
+	}
+	defer ln.Close()
+	fmt.Println("Ready to listen to connections")
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting")
+		} else {
+			newConnCh <- conn
+		}
+	}
+}
+
+func clientListener(inCommingMsgCh chan<- messages.ClientPayload, conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 2048)
+	var msg messages.ClientPayload
+
+	for {
+		bytes, err := conn.Read(buffer)
+
+		if err != nil {
+			fmt.Println("Error receiving, closing connection")
+			return
+		}
+		error := json.Unmarshal(buffer[:bytes], msg)
+		if error != nil {
+			fmt.Println("Error Unmarshall", err)
+			continue
+		}
+		inCommingMsgCh <- msg
+	}
 }
